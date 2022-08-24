22dd8721cbbedb0c4d775cc566f4513b93465e59
ekta <ektamo0123@gmail.com>
2022-08-24 13:45:28 +0530

    Added : first commit

---
+++ Gambler.java
@@ -0,0 +1,10 @@
+package gamblingSimulation;
+
+public class Gambler {
+	static int Stake = 100;
+	static int Bet = 1;
+	
+public static void main(String[] args) {
+	System.out.println("Gambling Simulation");		
+	}
+}
