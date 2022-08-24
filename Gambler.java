b51675821be820efc29eda25ca9a2ea1547645a0
ekta <ektamo0123@gmail.com>
2022-08-24 14:24:17 +0530

    Added : win or lose

--- Gambler.java
+++ Gambler.java
@@ -3,8 +3,18 @@ package gamblingSimulation;
 public class Gambler {
 	static int Stake = 100;
 	static int Bet = 1;
+	static int win = 1;
 	
 public static void main(String[] args) {
-	System.out.println("Gambling Simulation");		
+	System.out.println("Gambling Simulation");
+	int result = (int) (Math.random() * 10) % 2;
+	if (result == win) {
+		Stake++;
+		System.out.println("WIN");
+	} else {
+		Stake--;
+		System.out.println("LOSS");
+	}
+	System.out.println(Stake);
 	}
 }
